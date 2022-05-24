5e2273b3466c1723d4499b4b8136135736f6cce2
Roee Furman <roeefurman1@gmail.com>
2022-05-24 14:14:08 +0300

    STYLE: background

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,29 +1,31 @@
 <template>
   <section class="about">
-    <div class="main-title white big">
-      Hi! I am Roee Furman
-      <!-- <img src=".././assets/img/pp.jpg" class="my-pic" /> -->
-    </div>
-    <div class="main-text">
-      <span class="main-title white">Full-Stack Web Developer</span>
-      <!-- <p class="main-text"> -->
-      with experience in writing single/multi-page-applications using the latest
-      WEB technologies (Vue.js, Node.js, React.js, etc.). Graduate of the Coding
-      Academy - 640 hours of an intensive coding bootcamp that qualifies
-      Full-Stack developers.
-      <div class="contact">
-        <a target="_blank" href="https://www.facebook.com/roee.furman/">
-          <i class="fa fa-facebook"></i>
-        </a>
-        <a
-          target="_blank"
-          href="https://www.linkedin.com/in/roee-furman-16164711a/"
-        >
-          <i class="fa fa-linkedin"></i>
-        </a>
-        <a target="_blank" href="https://github.com/RoeeFurman">
-          <i class="fa-brands fa-github"></i>
-        </a>
+    <div class="bg">
+      <div class="main-title white big">
+        Hi! I am Roee Furman
+        <img src=".././assets/img/pp.jpg" class="my-pic" />
+      </div>
+      <div class="main-text">
+        <span class="main-title white">Full-Stack Web Developer</span>
+        <!-- <p class="main-text"> -->
+        with experience in writing single/multi-page-applications using the
+        latest WEB technologies (Vue.js, Node.js, React.js, etc.). Graduate of
+        the Coding Academy - 640 hours of an intensive coding bootcamp that
+        qualifies Full-Stack developers.
+        <div class="contact">
+          <a target="_blank" href="https://www.facebook.com/roee.furman/">
+            <i class="fa fa-facebook"></i>
+          </a>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/roee-furman-16164711a/"
+          >
+            <i class="fa fa-linkedin"></i>
+          </a>
+          <a target="_blank" href="https://github.com/RoeeFurman">
+            <i class="fa-brands fa-github"></i>
+          </a>
+        </div>
       </div>
     </div>
   </section>
